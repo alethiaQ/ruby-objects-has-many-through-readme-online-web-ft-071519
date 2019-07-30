bdea4251a1944831415aa8e8f26bdc3a76dbc04d
Alethia Quintero <alethiaq22@gmail.com>
2019-07-30 15:21:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -8,8 +8,13 @@ class Waiter
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
 
 end
